7f6c5d2f3389b58117e9c7a52c813d018c59d44c
unknown <da4adindu@gmail.com>
2022-03-10 23:32:37 +0000

    java basics

---
+++ Tree.java
@@ -0,0 +1,22 @@
+/*
+Planting a Tree
+Introduce yourself to users and plant a tree for them!
+*/
+
+// Define your tree class in this file!
+public class Tree {
+
+  public static void main(String[] args) {
+
+  System.out.println("Hey there, I’m David and I’m learning to code in Java!");
+
+  System.out.println("I’m going to plant a tree today!");
+  System.out.println("   ***  ");
+  System.out.println("   ***  ");
+  System.out.println("  *****  ");
+  System.out.println("    *    ");
+  System.out.println("    *    ");
+  System.out.println(" ####### "); 
+
+  }
+}
